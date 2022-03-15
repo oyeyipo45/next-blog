29cf86c3bee0582a7a84e613c24fe110e095a04d
damilola <oyeyipo45@gmail.com>
2022-03-15 13:17:37 +0100

    dynamic page names

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,33 @@
 import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+      <Layout>
+           <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
 }
 
-import { getAllPostIds } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
